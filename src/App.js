dabea08f6e781f1e80b3ddf5ce1549fe97a3049c
Todor Vlasev <todor.vlasev@strypes.eu>
2020-04-27 22:02:00 +0300

    adding routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component';
+
 import './App.css';
+
+import HomePage from './pages/homepage/homepage.component';
 
 const HatsPage = () => (
   <div>
